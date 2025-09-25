157c27b1c9a62bd438b6c02dcbbac082396c9376
urwill <urwill67@gmail.com>
2025-09-25 13:15:55 +0200

    Wayback machine availability check
    Vor Erzeugung eines neuen Eintrags in die Wayback machine prüfen, ob bereits einer existiert. Gerade bei neuen Einträgen ist das zwar leider nicht sehr zuverlässig, aber es sollte zumindest ein wenig die Ressourcen schonen

--- scripts/wayback.js
+++ scripts/wayback.js
@@ -12,16 +12,36 @@ async function archiveLinksSequentially() {
 
     const currentUrl = archiveQueue.shift();
     console.log("Archivierung gestartet.", currentUrl);
-    let iframe = document.createElement("iframe");
-    iframe.style.display = "none";
-    //iframe.style.width = "1500px";
-    //iframe.style.height = "800px";
-    iframe.src = "https://web.archive.org/save/" + encodeURIComponent(currentUrl);
-    iframe.onload = function () {
-        document.body.removeChild(iframe);
-        console.log("Archivierung fertig.", currentUrl);
-        isArchiving = false;
-        archiveLinksSequentially();
-    };
-    document.body.appendChild(iframe);
+
+    // Zuerst prüfen, ob schon archiviert
+    fetch("https://archive.org/wayback/available?url=" + encodeURIComponent(currentUrl))
+        .then(r => r.json())
+        .then(data => {
+            console.log(data);
+            const exists = data.archived_snapshots && data.archived_snapshots.closest && data.archived_snapshots.closest.available;
+            if (exists) {
+                console.log("Bereits archiviert:", data.archived_snapshots.closest.url);
+                isArchiving = false;
+                archiveLinksSequentially();
+            } else {
+                console.log("Noch nicht archiviert, also jetzt iFrame anlegen.", currentUrl);
+                let iframe = document.createElement("iframe");
+                iframe.style.display = "none";
+                //iframe.style.width = "1500px";
+                //iframe.style.height = "800px";
+                iframe.src = "https://web.archive.org/save/" + encodeURIComponent(currentUrl);
+                iframe.onload = function () {
+                    document.body.removeChild(iframe);
+                    console.log("Archivierung fertig.", currentUrl);
+                    isArchiving = false;
+                    archiveLinksSequentially();
+                };
+                document.body.appendChild(iframe);
+            }
+        })
+        .catch(err => {
+            console.error("Fehler bei der Wayback-Abfrage:", err);
+            isArchiving = false;
+            archiveLinksSequentially(); // Fehler überspringen und weitermachen
+        });
 }
